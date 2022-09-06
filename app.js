61a50712c480773e801e837e39e8d88e5a5a25d6
Jeremias Zec <jeremiaszec@gmail.com>
2022-09-05 23:48:44 -0300

    sistema de descuentos con objetos y arrays

--- app.js
+++ app.js
@@ -8,62 +8,83 @@ variantes a considerar:
 -cantidad de servicios que incorpora cada cliente
 */ 
 
-const precioMensual = 589 /*mas IVA*/
+class Servicio{
+    #public;
+    constructor(precio, descuentoSemestral, descuentoAnual, descuentoEfectivo){
+        this.precio = precio;
+        this.descuentoSemestral = descuentoSemestral;
+        this.descuentoAnual = descuentoAnual;
+        this.descuentoEfectivo = descuentoEfectivo;
+        this.formaDePago;
+        this.periodo;
+    }
 
-const descuentoEfectivo = 0.85
-const descuentoSemestral = 0.9
-const descuentoAnual = 0.8
+    contratar(periodo, formaDePago){
+        this.periodo = periodo;
+        this.formaDePago = formaDePago;
+    }
 
-function precioFinal(precio, meses, formaDePago){    
-    precioPrevio = descuento(precio, meses, formaDePago);
-    return precioPrevio*1.21;
+    descuento(){ 
+        return this.descuentoForma(this.descuentoPeriodo());
+    }
+ 
+    precioFinal(){    
+        let precioPrevio = this.descuento(this.periodo, this.formaDePago);
+        return precioPrevio*1.21;
+    }
+     
+    
+    descuentoForma(precio){
+        switch (this.formaDePago){
+            case 'tarjeta':
+                return precio;
+            break;
+    
+            case 'efectivo':
+                return precio*this.descuentoEfectivo;
+            break;
+    
+            default:
+                console.log("Error: la forma de pago elegida es incorrecta")
+                return 0;
+            break;
+        }
+    }
+    descuentoPeriodo(){
+        let auxiliar = 0;
+    
+        if(this.periodo < 6){
+            auxiliar = this.precio*this.periodo;
+        }
+        else if(this.periodo >= 6 && this.periodo < 12){
+            auxiliar = this.precio*this.periodo*this.descuentoSemestral;
+        }
+        else if (this.periodo >= 12){
+            auxiliar = this.precio*this.periodo*this.descuentoAnual;
+        }
+        return auxiliar;
+    }
 }
 
-function descuentoPeriodo(precio, periodo){
-    let auxiliar = 0;
-
-    if(periodo < 6){
-        auxiliar = precio*periodo;
-    }
-    else if(periodo >= 6 && periodo < 12){
-        auxiliar = precio*periodo*descuentoSemestral;
-    }
-    else if (periodo >= 12){
-        auxiliar = precio*periodo*descuentoAnual;
-    }
-    return auxiliar;
+function precioTotal(servicios){
+    let total=0;
+    servicios.forEach(function(servicio){
+        total = total + servicio.precioFinal();
+    })
+    return total;
 }
 
-function descuentoForma(precio, formaDePago){
-    switch (formaDePago){
-        case 'tarjeta':
-            return precio;
-        break;
+let servicioA = new Servicio(589, 0.9, 0.8, 0.85); 
+let servicioB = new Servicio(1000, 0.95, 0.9, 0.85);
 
-        case 'efectivo':
-            return precio*descuentoEfectivo;
-        break;
+servicioA.contratar(14, 'efectivo'); //se contrata por 14 meses y se paga en efectivo
+servicioB.contratar(6, 'tarjeta');  //se contrata por 6 meses y se paga con tarjeta
 
-        default:
-            console.log("Error: la forma de pago elegida es incorrecta")
-            return 0;
-        break;
-    }
-}
+let carrito = [];
+carrito.push(servicioA, servicioB);
 
-function descuento (precio, periodo, formaDePago){ 
-    return descuentoForma(descuentoPeriodo(precio, periodo), formaDePago);
-}
-
-console.log("Bienvenido a la a LaEmpresa")
-let cantidadMeses = Number(prompt("ingrese la cantidad de meses que desea suscrbirse"))
-let formaDePago = prompt("ingrese una forma de pago: tarjeta|efectivo")
-console.log(`El precio final es: ${precioFinal(precioMensual, cantidadMeses, formaDePago)}`)
-
-
-
-
-
+console.log("Bienvenido a la a LaEmpresa");
+console.log(`El precio final del carrito es: ${precioTotal(carrito)}`);
 
 
 
