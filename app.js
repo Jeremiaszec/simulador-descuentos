e548b5adaeb237ca69c003744e34d9434c8977f6
jeremias zec <jeremiaszec@gmail.com>
2023-06-03 11:36:08 -0300

    se quita el codigo JS del HTML

--- app.js
+++ app.js
@@ -8,12 +8,35 @@ variantes a considerar:
 -cantidad de servicios que incorpora cada cliente
 */ 
 
+
+
+/* a esto se lo llama funcion constructora y es una de las formas de definir un objeto */
 function Carrito(){
     this.servicios = [];
     this.pagado = false;
 
     this.agregar = function(servicio){
         this.servicios.push(servicio);
+        console.log(this.servicios);
+    }
+
+    this.quitar = function(nombreServicio){
+
+        console.log(`${servicios[0].nombre} === ${nombreServicio}`); 
+        console.log(servicios[0].nombre === nombreServicio);
+
+
+        var index = this.servicios.findIndex(function(servicio){
+            return servicio === nombreServicio;
+        }); 
+
+        console.log(index);
+        console.log(this.servicios.length);
+        
+        if(index > -1){
+            this.servicios.splice(index, 1);
+        }
+        console.log(this.servicios);
     }
 
     this.total = function(){
@@ -29,6 +52,8 @@ function Carrito(){
     }
 }
 
+
+/* Esta es la forma clasica de definir un objeto */
 class Servicio{
     #public;
     constructor(nombre, img,  precio, descuentoSemestral, descuentoAnual, descuentoEfectivo){
@@ -96,39 +121,52 @@ let servicioA = new Servicio("Instalacion de camaras IP", "img/camara_IP_262x262
 let servicioB = new Servicio("Monitorizacion de calderas", "img/caldera_262x262.png", 0.95, 0.9, 0.85);
 let servicioC = new Servicio("Instalacion de Motores", "img/motor_dc_262x262.png", 0.95, 0.9, 0.85);
 
-// servicioA.contratar(14, 'efectivo'); //se contrata por 14 meses y se paga en efectivo
-// servicioB.contratar(6, 'tarjeta');  //se contrata por 6 meses y se paga con tarjeta
+//servicioA.contratar(14, 'efectivo'); //se contrata por 14 meses y se paga en efectivo
+//servicioB.contratar(6, 'tarjeta');  //se contrata por 6 meses y se paga con tarjeta
 
-// let carrito = new Carrito();
+let carrito = new Carrito();
+let servicios = [servicioA, servicioB, servicioC];
 
-// carrito.agregar(servicioA);
-// carrito.agregar(servicioB);
+//carrito.agregar(servicioA);
+//carrito.agregar(servicioB);
 
 // console.log("Bienvenido a la a LaEmpresa");
 // console.log(`El precio final del carrito es: ${carrito.total()}`);
 
 // carrito.pagar();
 
-let servicios = [servicioA, servicioB, servicioC];
-
 let padre = document.getElementById("cards")
 padre.className = "row"
 
-let html_text = ""; 
 for (servicio of servicios) {
-    html_text = html_text +
-    `<div class="card m-2" style="width: 18rem;">
-        <img src=${servicio.img} class="card-img-top" alt="osito de peluche">
-        <div class="card-body">
-            <h5 class="card-title">${servicio.nombre}</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" onclick="changeText(this)" class="btn btn-primary">Agregar a el carrito</a>
- 
-        </div>
-    </div>`;
-}
-padre.innerHTML = html_text;
+    let html_card = document.createElement("div")
+    html_card.className = "card m-2";
+    html_card.style.width = "18rem";
+    html_card.innerHTML =
+    `
+    <img src=${servicio.img} class="card-img-top" alt="osito de peluche">
+    <div class="card-body">
+        <h5 class="card-title">${servicio.nombre}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" onclick="respuestaClick(this, '${servicio.nombre}')" class="btn btn-primary">Agregar a el carrito</a>
+    </div>
+    `;
 
+    //let button = html_card.querySelector("div a");
+    
+    padre.appendChild(html_card)
+}
+
+function respuestaClick(node, nombreServicio) {   
+    if(node.textContent =='Agregar a el carrito'){
+        carrito.agregar(nombreServicio);
+        node.textContent = "Agregado";
+    }
+    else{
+        carrito.quitar(nombreServicio);
+        node.textContent = "Agregar a el carrito";
+    }
+}
 
 /***************************** FIN **********************************/
 
